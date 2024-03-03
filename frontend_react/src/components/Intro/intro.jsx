9e733276c0ff3824c159a16ee98739593350320b
taduchuy04@gmail.com <taduchuy04@gmail.com>
2024-03-03 12:54:44 -0500

    fix hanger

--- frontend_react/src/components/Intro/intro.jsx
+++ frontend_react/src/components/Intro/intro.jsx
@@ -10,7 +10,7 @@ const intro = () => {
           <span className="locationText">
             <span>Located in Montreal Canada</span>
           </span>
-          <div className="btnLocationWrap">
+          <div className="app__flex btnLocationWrap">
             <div className="btnLocation">
               <a
                 href="https://www.google.com/maps/place/Montreal,+QC/@45.5587087,-74.0415125,10z/data=!3m1!4b1!4m6!3m5!1s0x4cc91a541c64b70d:0x654e3138211fefef!8m2!3d45.5018869!4d-73.5673919!16zL20vMDUycDc?entry=ttu"
